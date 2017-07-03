0772c668ec05caaf38dcfa1b1904354a641f8ccb
Samuel Reed <samuel.trace.reed@gmail.com>
2017-07-03 10:36:33 -0500

    feat(python-swaggerpy): Addl examples & usage

--- official-http/python-swaggerpy/bitmexClient.py
+++ official-http/python-swaggerpy/bitmexClient.py
@@ -3,6 +3,7 @@
 from bravado.client import SwaggerClient
 from bravado.requests_client import RequestsClient
 from BitMEXAPIKeyAuthenticator import APIKeyAuthenticator
+import pprint
 
 HOST = "https://testnet.bitmex.com"
 SPEC_URI = HOST + "/api/explorer/swagger.json"
@@ -13,27 +14,36 @@ config = {
   'use_models': False,
   # This library has some issues with nullable fields
   'validate_responses': False,
+  # Returns response in tuple
+  'also_return_response': True,
 }
 
 bitMEX = SwaggerClient.from_url(
   SPEC_URI,
   config=config)
 
+pp = pprint.PrettyPrinter(indent=2)
+
 # You can get a feel for what is available by printing these objects.
 # See also https://testnet.bitmex.com/api/explorer
+print('---The BitMEX Object:---')
 print(dir(bitMEX))
+print('\n---The BitMEX.Trade Object:---')
 print(dir(bitMEX.Trade))
 
 # Basic unauthenticated call
-res = bitMEX.Trade.Trade_get(symbol='XBTUSD', count=40).result()
-print(res)
+res, http_response = bitMEX.Trade.Trade_get(symbol='XBTUSD', count=1).result()
+print('\n---A basic Trade GET:---')
+pp.pprint(res)
+print('\n---Response details:---')
+print("Status Code: %d, headers: %s" % (http_response.status_code, http_response.headers))
 
 
 #
 # Authenticated calls
 #
 # To do authentication, you must generate an API key.
-# Do so at https:https://testnet.bitmex.com/app/apiKeys
+# Do so at https://testnet.bitmex.com/app/apiKeys
 
 API_KEY = '<API_KEY_HERE>'
 API_SECRET = '<API_SECRET_HERE>'
@@ -48,6 +58,13 @@ bitMEXAuthenticated = SwaggerClient.from_url(
 
 print(dir(bitMEXAuthenticated.Position))
 
-# Basic unauthenticated call
+# Basic authenticated call
+print('\n---A basic Position GET:---')
 res = bitMEXAuthenticated.Position.Position_get().result()
-print(res)
+pp.pprint(res)
+
+
+# Basic order placement
+# print(dir(bitMEXAuthenticated.Order))
+# res = bitMEXAuthenticated.Order.Order_new(symbol='XBTUSD', orderQty=3, price=1000).result()
+# print(res)
