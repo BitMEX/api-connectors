01aecc4a16586cab0ec46980990824deeed2a61b
Ryan Fox <ryan@foxrow.com>
2018-01-16 12:03:43 -0600

    fixed typo in project url

--- official-http/python-swaggerpy/setup.py
+++ official-http/python-swaggerpy/setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 
 setup(name='bitmex',
-      version='0.2.1',
+      version='0.2.2',
       description='BitMEX API client.',
       author='Samuel Reed',
       author_email='sam@bitmex.com',
-      url='https://github.com/bitmex/apiConnectors',
+      url='https://github.com/bitmex/api-Connectors',
       install_requires=['bravado'],
       packages=[''],
       )
